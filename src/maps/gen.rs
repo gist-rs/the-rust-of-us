e7e934fdea6bf570810fbe14092cd6090c2b8459
katopz <katopz@gmail.com>
2024-10-05 09:19:53 +0900

    fix: conflict placement

--- src/maps/gen.rs
+++ src/maps/gen.rs
@@ -2,7 +2,8 @@ use crate::core::{
     map::{find_path, generate_map, MapPosition, PathCost},
     scene::GameMap,
 };
-use rand::Rng;
+use anyhow::Result;
+use rand::{rngs::OsRng, Rng};
 
 fn always_find_path(start: MapPosition, goal: MapPosition) -> PathCost {
     find_path(
@@ -14,6 +15,40 @@ fn always_find_path(start: MapPosition, goal: MapPosition) -> PathCost {
     .unwrap()
 }
 
+fn check_and_pave_path(
+    walkables: &mut Vec<Vec<bool>>,
+    map: &mut Vec<Vec<String>>,
+    start: MapPosition,
+    main_route_path: &[(usize, usize)],
+    target_char: &str,
+    rng: &mut rand::rngs::ThreadRng,
+) {
+    for row in 0..8 {
+        for col in 0..8 {
+            if map[row][col] == *target_char {
+                let target = MapPosition { x: col, y: row };
+                if let Ok(_path) = find_path(walkables, start.clone().to_tuple(), (col, row), false)
+                {
+                    // OK
+                } else {
+                    // If no sub-route found, randomly pick a node and pave the way
+                    let node_index = rng.gen_range(1..main_route_path.len() - 1);
+                    let (x, y) = main_route_path[node_index];
+
+                    // Pave the way to the nearest target
+                    let path_cost = always_find_path(MapPosition { x, y }, target.clone());
+                    for (px, py) in path_cost.path {
+                        if !walkables[py][px] {
+                            walkables[py][px] = true;
+                            "➖".clone_into(&mut map[py][px]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 // Refine the walkable map
 fn refine_walkable_map(
     walkables: &mut Vec<Vec<bool>>,
@@ -22,7 +57,6 @@ fn refine_walkable_map(
     goal: MapPosition,
 ) -> GameMap {
     let mut rng = rand::thread_rng();
-    let found_sub_route = false;
     let GameMap(map) = game_map;
 
     // Check if any node in the main route can walk from start to goal
@@ -43,66 +77,18 @@ fn refine_walkable_map(
         }
     };
 
-    // Check if any node in the main route can access 💰
-    for row in 0..8 {
-        for col in 0..8 {
-            if map[row][col] == *"💰" {
-                let target = MapPosition { x: col, y: row };
-                if let Ok(_path) = find_path(walkables, start.clone().to_tuple(), (col, row), false)
-                {
-                    // OK
-                } else {
-                    // If no sub-route found, randomly pick a node and pave the way
-                    if !found_sub_route {
-                        let node_index = rng.gen_range(1..main_route_path.len() - 1);
-                        let (x, y) = main_route_path[node_index];
+    // Check and pave paths to "💰"
+    check_and_pave_path(
+        walkables,
+        map,
+        start.clone(),
+        &main_route_path,
+        "💰",
+        &mut rng,
+    );
 
-                        // Pave the way to the nearest treasure
-                        let path_cost = always_find_path(MapPosition { x, y }, target.clone());
-                        for (px, py) in path_cost.path {
-                            if !walkables[py][px] {
-                                walkables[py][px] = true;
-                                "➖".clone_into(&mut map[py][px]);
-                            }
-                        }
-                    } else {
-                        break;
-                    }
-                }
-            }
-        }
-
-        // Check if any node in the main route can access 💀
-        for row in 0..8 {
-            for col in 0..8 {
-                if map[row][col] == *"💀" {
-                    let target = MapPosition { x: col, y: row };
-                    if let Ok(_path) =
-                        find_path(walkables, start.clone().to_tuple(), (col, row), false)
-                    {
-                        // OK
-                    } else {
-                        // If no sub-route found, randomly pick a node and pave the way
-                        if !found_sub_route {
-                            let node_index = rng.gen_range(1..main_route_path.len() - 1);
-                            let (x, y) = main_route_path[node_index];
-
-                            // Pave the way to the nearest treasure
-                            let path_cost = always_find_path(MapPosition { x, y }, target.clone());
-                            for (px, py) in path_cost.path {
-                                if !walkables[py][px] {
-                                    walkables[py][px] = true;
-                                    "➖".clone_into(&mut map[py][px]);
-                                }
-                            }
-                        } else {
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-    }
+    // Check and pave paths to "💀"
+    check_and_pave_path(walkables, map, start, &main_route_path, "💀", &mut rng);
 
     game_map.clone()
 }
@@ -110,49 +96,51 @@ fn refine_walkable_map(
 #[allow(clippy::type_complexity)]
 pub fn gen_map_from_public_key(
     pubkey: &str,
-) -> anyhow::Result<(Vec<Vec<bool>>, MapPosition, MapPosition, GameMap)> {
-    let mut map = vec![vec!['➖'.to_string(); 8]; 8];
+) -> Result<(Vec<Vec<bool>>, MapPosition, MapPosition, GameMap)> {
+    let mut map = vec![vec!["➖".to_string(); 8]; 8];
 
     // Fill the edges with 🌳
     for i in 0..8 {
-        map[0][i] = '🌳'.to_string();
-        map[7][i] = '🌳'.to_string();
-        map[i][0] = '🌳'.to_string();
-        map[i][7] = '🌳'.to_string();
+        map[0][i] = "🌳".to_string();
+        map[7][i] = "🌳".to_string();
+        map[i][0] = "🌳".to_string();
+        map[i][7] = "🌳".to_string();
     }
 
     // Place 🌳 based on the rest of the characters
     for (i, ch) in pubkey.chars().enumerate().skip(2) {
         let row = (i % 7) + 1; // Rows 1 to 7
         let col = ch as u8 % 8;
-        map[row][col as usize] = '🌳'.to_string();
-    }
-
-    // Place 💰 randomly between row 1-3, col 2-6
-    let mut rng = rand::thread_rng();
-    for _ in 0..3 {
-        let row = rng.gen_range(1..=3);
-        let col = rng.gen_range(2..=6);
-        map[row][col] = '💰'.to_string();
-    }
-
-    // Place 💀 randomly between row 4-6, col 2-6
-    for _ in 0..3 {
-        let row = rng.gen_range(4..=6);
-        let col = rng.gen_range(2..=6);
-        map[row][col] = '💀'.to_string();
+        map[row][col as usize] = "🌳".to_string();
     }
 
     // Place the 🚪 gates
-    #[allow(clippy::iter_nth_zero)]
     let c = 1 + pubkey.chars().nth(0).unwrap() as u8 % 6;
     let a = 1 + pubkey.chars().nth(1).unwrap() as u8 % 6;
-    map[0][c as usize] = '🚪'.to_string();
-    map[7][a as usize] = '🚪'.to_string();
+    map[0][c as usize] = "🚪".to_string();
+    map[7][a as usize] = "🚪".to_string();
 
     // Place 🆒 and 🆕
-    map[1][c as usize] = '🆒'.to_string();
-    map[6][a as usize] = '🆕'.to_string();
+    let mut rng = OsRng;
+    loop {
+        // Place 💰 and 💀 randomly ensuring no conflict with 🆒 and 🆕
+        for _ in 0..1 {
+            let row = rng.gen_range(1..=6);
+            let col = rng.gen_range(1..=6);
+            map[row][col] = "💰".to_string();
+        }
+        for _ in 0..1 {
+            let row = rng.gen_range(1..=6);
+            let col = rng.gen_range(1..=6);
+            map[row][col] = "💀".to_string();
+        }
+
+        if map[1][c as usize] != "💰" && map[1][c as usize] != "💀" {
+            map[1][c as usize] = "🆒".to_string();
+            map[6][a as usize] = "🆕".to_string();
+            break;
+        }
+    }
 
     let (walkables, start, goal) = generate_map(&map);
 
